cb4a2e7a279ffca160fc6d00bdcf2a8d507fe619
Prem <premkamban@gmail.com>
2020-08-17 18:52:20 +0530

    Initial commit

---
+++ src/registrationForm.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { FormControl, InputLabel, Input, FormHelperText } from '@material-ui/core';
+
+const FormPage = () => {
+    return (
+        <FormControl>
+            <InputLabel htmlFor="my-input">Email address</InputLabel>
+            <Input id="my-input" aria-describedby="my-helper-text" />
+            <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+            <InputLabel htmlFor="my-input">Email address</InputLabel>
+            <Input id="my-input" aria-describedby="my-helper-text" />
+            <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+            <InputLabel htmlFor="my-input">Email address</InputLabel>
+            <Input id="my-input" aria-describedby="my-helper-text" />
+            <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+            <InputLabel htmlFor="my-input">Email address</InputLabel>
+            <Input id="my-input" aria-describedby="my-helper-text" />
+            <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+            <InputLabel htmlFor="my-input">Email address</InputLabel>
+            <Input id="my-input" aria-describedby="my-helper-text" />
+            <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+            <InputLabel htmlFor="my-input">Email address</InputLabel>
+            <Input id="my-input" aria-describedby="my-helper-text" />
+            <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+            <InputLabel htmlFor="my-input">Email address</InputLabel>
+            <Input id="my-input" aria-describedby="my-helper-text" />
+            <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+
+
+        </FormControl>
+    );
+};
+
+export default FormPage;
